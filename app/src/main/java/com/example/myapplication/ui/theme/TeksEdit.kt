0be72ef1488f957d0da7735029710ca4745154bb
dianfitrip <dianfitri683@gmail.com>
2025-10-31 09:15:22 +0700

    Daftar opsi untuk Radio Button Jenis Kelamin(Laki-laki/Perempuan)

--- app/src/main/java/com/example/myapplication/ui/theme/TeksEdit.kt
+++ app/src/main/java/com/example/myapplication/ui/theme/TeksEdit.kt
@@ -15,4 +15,7 @@ fun FormDataDiri(modifier: Modifier = Modifier
     var nama by remember {mutableStateOf("")}
     var alamat by remember {mutableStateOf("")}
     var jenis by remember {mutableStateOf("")}
+
+    // Daftar opsi untuk Radio Button Jenis Kelamin
+    val gender: List<String> = listOf("Laki-laki","Perempuan")
   }
