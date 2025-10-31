216d9ea62f654cc99ec0208d08bcad55ba203ea7
dianfitrip <dianfitri683@gmail.com>
2025-10-31 09:39:24 +0700

    Divider atau garis pembatas horizontal yang ke-2 sebagai pemisah tombol submit dan card(untuk menampilkan hasil input yg disubmit)

--- app/src/main/java/com/example/myapplication/ui/theme/TeksEdit.kt
+++ app/src/main/java/com/example/myapplication/ui/theme/TeksEdit.kt
@@ -77,5 +77,15 @@ fun FormDataDiri(modifier: Modifier = Modifier
         ){
             Text(stringResource( R.string.submit))
         }
+
+        HorizontalDivider(
+            modifier = Modifier
+                .padding(bottom = dimensionResource(R.dimen.padding_medium),
+                    top = dimensionResource(
+                        id = R.dimen.padding_medium
+                    )),
+            thickness = dimensionResource(R.dimen.divider_tipis),
+            color = Color.DarkGray
+        )
     }
   }
