e29195297fa7ddb0bde95f595297ffb2bc074162
dianfitrip <dianfitri683@gmail.com>
2025-10-31 09:14:16 +0700

    variabel-variabel untuk menyimpan data yang diperoleh dari komponen UI

--- app/src/main/java/com/example/myapplication/ui/theme/TeksEdit.kt
+++ app/src/main/java/com/example/myapplication/ui/theme/TeksEdit.kt
@@ -5,8 +5,14 @@ package com.example.myapplication.ui.theme
 
 @Composable
 fun FormDataDiri(modifier: Modifier = Modifier
-){
+) {
     // variabel-variabel untuk mengingat nilai masukan dari keyboard
-    var textNama by remember {mutableStateOf("")}
-    var textAlamat by remember {mutableStateOf("")}
-    var textJK by remember {mutableStateOf("")}
+    var textNama by remember { mutableStateOf("") }
+    var textAlamat by remember { mutableStateOf("") }
+    var textJK by remember { mutableStateOf("") }
+
+    //variabel-variabel untuk menyimpan data yang diperoleh dari komponen UI
+    var nama by remember {mutableStateOf("")}
+    var alamat by remember {mutableStateOf("")}
+    var jenis by remember {mutableStateOf("")}
+  }
