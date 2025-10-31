71ad9ab0256936b86d886e37c7b30d414321a565
dianfitrip <dianfitri683@gmail.com>
2025-10-31 09:22:50 +0700

    Row untuk menampung pilihan Jenis Kelamin (RadioButton)

--- app/src/main/java/com/example/myapplication/ui/theme/TeksEdit.kt
+++ app/src/main/java/com/example/myapplication/ui/theme/TeksEdit.kt
@@ -32,5 +32,11 @@ fun FormDataDiri(modifier: Modifier = Modifier
                 textNama = it
             }
         )
+        Row {
+            gender.forEach { item ->
+                Row(modifier = Modifier.selectable(
+                    selected = textJK == item,
+                    onClick = { textJK = item }
+                ), verticalAlignment = Alignment.CenterVertically) {
     }
   }
