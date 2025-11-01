48235fa04f0cabc830e2b724c557f21e19708fc8
dianfitrip <dianfitri683@gmail.com>
2025-11-02 02:53:51 +0700

    PR: variabel-variabel untuk mengingat nilai yang dimasukan dari keyboard

--- app/src/main/java/com/example/myapplication/TeksEditPR.kt
+++ app/src/main/java/com/example/myapplication/TeksEditPR.kt
@@ -4,5 +4,9 @@ package com.example.myapplication
 
 @Composable
 fun FormDataDiriPR(modifier: Modifier = Modifier){
-
+    // State untuk input
+    var textNama by remember { mutableStateOf("") }
+    var textAlamat by remember { mutableStateOf("") }
+    var textJK by remember { mutableStateOf("") }
+    var textStatus by remember { mutableStateOf("") }
 }
